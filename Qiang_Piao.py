cd4086077b6e685957a5b30710adf58212e035d8
deming <anong9420@gmail.com>
2019-03-23 04:41:01 -0400

    except for the quantity and time func the first page done

---
+++ Qiang_Piao.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
+from fun import se
+
+class buy_ticket():
+    def __init__(self, departure, depart_id, destination, dest_id, date, quantity):
+	
+        
+        driver.get("https://www.amtrak.com/home")
+        self.departure = departure
+        self.depart_id = depart_id
+        self.destination = destination
+        self.dest_id = dest_id
+        #MM/DD/YY
+        self.date = date
+        self.quantity = quantity
+
+    def book_ticket(self):
+
+        se.send_keys_by_xpath(driver, '//*[@id="page-content"]/section[2]/div[2]/article/div[2]/div/div[5]/form/div/div[3]/div[1]/div[2]/div/div[1]/div/div/div/input[2]', self.departure)
+        time.sleep(1)
+        se.click_by_xpath(driver, '//*[@id="WAS"]')
+
+        se.send_keys_by_xpath(driver, '//*[@id="page-content"]/section[2]/div[2]/article/div[2]/div/div[5]/form/div/div[3]/div[1]/div[2]/div/div[2]/div/div/div/input[2]', self.destination)
+        time.sleep(1)
+        se.click_by_id(driver, self.dest_id)
+
+        se.send_keys_by_xpath(driver, '//*[@id="page-content"]/section[2]/div[2]/article/div[2]/div/div[5]/form/div/div[3]/div[1]/div[3]/div[1]/div/div[1]/div/div[1]/div/div[2]/input', self.date)
+        
+        se.click_by_xpath(driver, '//*[@id="page-content"]/section[2]/div[2]/article/div[2]/div/div[5]/form/div/div[3]/div[1]/div[4]/div[1]/div[2]')
+        #se.send_keys_by_name(driver, '/sessionWorkflow/productWorkflow[@product='Rail']/tripRequirements/allJourneyRequirements/@adultTravellers', self.quantity)
+        se.click_by_id(driver, 'findtrains')
+
+    def main(self):
+        self.book_ticket()
+
+if __name__=='__main__':
+    
+    begin = time.time()
+    driver = webdriver.Firefox()
+    obj = buy_ticket('Washington, DC', 'WAS', 'Boston, MA', 'BOS', '03/28/2019', '2')
+    obj.main()
+    end = time.time()
+    T = int(end-begin)
+    #print("total_time = %d",%(T))	
